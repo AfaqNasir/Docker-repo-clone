0e9ce45057dc91caa8441948ff8b101e71d349cc
Muhammad Afaq Nasir <48288031+AfaqNasir@users.noreply.github.com>
2023-10-31 20:38:49 +0500

    Update push_all_images.sh

--- push_all_images.sh
+++ push_all_images.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Set the source and destination image repositories
-SOURCE_REPOSITORY="docker://"
-DESTINATION_REPOSITORY="docker://afaqnasir/"
+SOURCE_REPOSITORY=$1
+DESTINATION_REPOSITORY=$2
 
 # Get all the tags of the source image
-IMAGE_TAGS=$(skopeo inspect docker://sl | jq -r '.RepoTags[]')
+IMAGE_TAGS=$(skopeo inspect $SOURCE_REPOSITORY | jq -r '.RepoTags[]')
 
 # Copy all the tags of the source image to the destination repository
 for TAG in $IMAGE_TAGS; do
-  skopeo copy docker://sl:$TAG docker://afaqnasir/sl-bit:$TAG
+  skopeo copy $SOURCE_REPOSITORY:$TAG $DESTINATION_REPOSITORY:$TAG
 done
-
-
